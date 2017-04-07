b72166c9c95936b0750220569a8cd399de9b45fa
MIrko Sertic <mirko.sertic@web.de>
2017-04-07 17:07:23 +0200

    Further testing with TeaVM WASM JS Interaction

--- src/main/java/de/mirkosertic/Main.java
+++ src/main/java/de/mirkosertic/Main.java
@@ -1,56 +1,24 @@
 package de.mirkosertic;
 
-import org.teavm.interop.Export;
-import org.teavm.interop.Import;
-import org.teavm.jso.JSBody;
+public abstract class Main {
 
-public class Main {
+    public static final Main ONE = new Main() {};
+    public static final Main TWO = new Main() {};
+    public static final Main THREE = new Main() {};
 
-    @JSBody(params = "aValue", script = "console.print(aValue);")
-    @Import(module = "log", name = "log_int")
-    public static native void log(int aValue);
-
-    @JSBody(params = "aValue", script = "console.print(aValue);")
-    @Import(module = "log", name = "log_float")
-    public static native void log(float aValue);
-
-    @JSBody(params = "aValue", script = "console.print(aValue);")
-    @Import(module = "log", name = "log_double")
-    public static native void log(double aValue);
-
-    @JSBody(params = "aValue", script = "console.print(aValue);")
-    @Import(module = "log", name = "log_string")
-    public static native void log(String aValue);
-
-    @Export(name = "passThru")
-    public static int passThru(int aValue) {
-        log("called from javascript");
-        return aValue + 1;
-    }
-
-    @Import(module = "logic", name = "invokeMe")
-    public static native void invokeMe();
-
-    static class Test {
-        private final int value;
-
-        public Test(int value) {
-            this.value = value;
+    public static Main valueOf(String aValue) {
+        switch (aValue) {
+            case "THREE":
+                return THREE;
+            case "ONE":
+                return ONE;
+            case "TWO":
+                return TWO;
         }
+        throw new IllegalArgumentException();
     }
 
     public static void main(String[] args) {
-        int i = 12;
-        int j = 13;
-        int k = i + j;
-        Test test = new Test(11);
-        log(k);
-        log(10.344f);
-        log(10.344d);
-        log("Hello");
-        log("Sir");
-        log("DuD");
-        log(test.value);
-        invokeMe();
+        Main theInstance = valueOf("");
     }
 }
